de53f5c72278ebf4d47c420f43d62747d13a27a5
danielto1404 <daniilkorolev00@gmail.com>
2022-08-31 04:05:52 +0300

    add vision package

---
+++ tlayers/vision/__init__.py
@@ -0,0 +1,1 @@
+from .upsample_conv import UpsampleConv2d
---
+++ tlayers/vision/upsample_conv.py
@@ -0,0 +1,50 @@
+from typing import Union, Optional, Tuple
+
+from torch import nn
+
+
+class UpsampleConv2d(nn.Module):
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            kernel_size: Union[int, tuple[int, int]],
+            stride: Union[int, tuple[int, int]] = 1,
+            padding: Union[str, int, tuple[int, int]] = 0,
+            dilation: Union[int, tuple[int, int]] = 1,
+            groups: int = 1,
+            bias: bool = True,
+            padding_mode: str = "zeros",
+            size: Optional[Union[int, Tuple[int, ...]]] = None,
+            scale_factor: Optional[Union[float, tuple[float, ...]]] = None,
+            mode: str = 'nearest',
+            align_corners: Optional[bool] = None,
+            recompute_scale_factor: Optional[bool] = None,
+    ):
+        super(UpsampleConv2d, self).__init__()
+
+        self.upsample = nn.Upsample(
+            size,
+            scale_factor,
+            mode,
+            align_corners,
+            recompute_scale_factor
+        )
+
+        self.conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride,
+            padding,
+            dilation,
+            groups,
+            bias,
+            padding_mode
+        )
+
+    def forward(self, x):
+        x = self.upsample(x)
+        x = self.conv(x)
+
+        return x
